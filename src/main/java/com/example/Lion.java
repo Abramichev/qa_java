85cf3067d05099cd486bf4e44766675da1797e0c
Abramichev <endi79@mail.ru>
2023-02-05 17:29:57 +0300

    first commit

---
+++ src/main/java/com/example/Lion.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.util.List;
+
+public class Lion {
+
+    private Feline feline;
+
+    boolean hasMane;
+
+    public Lion(String sex, Feline feline) throws Exception {
+        this.feline=feline;
+        if ("Самец".equals(sex)) {
+            hasMane = true;
+        } else if ("Самка".equals(sex)) {
+            hasMane = false;
+        } else {
+            throw new Exception("Use valid animal sex values - male or female");
+        }
+    }
+
+
+    public int getKittens() {
+        return feline.getKittens();
+    }
+
+    public boolean doesHaveMane() {
+        return hasMane;
+    }
+
+    public List<String> getFood() throws Exception {
+        return feline.getFood("Хищник");
+    }
+}
